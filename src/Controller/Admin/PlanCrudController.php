c1e3038c8e3b233fcbbb7514b924afaa37196204
JeremieN6 <ngoyi.jeremie@gmail.com>
2025-09-03 18:03:37 +0200

    fix(PlanCrudController): import de DateField

--- src/Controller/Admin/PlanCrudController.php
+++ src/Controller/Admin/PlanCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Plan;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
