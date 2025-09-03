902291f776c72450052e3d67bcadfb9555e50bb1
JeremieN6 <ngoyi.jeremie@gmail.com>
2025-09-03 17:59:13 +0200

    feat(users): ajout de la methode __toString pour retourrner l'adresse mail dans les champs qui ont une jointure à la table user dans Easy Admin  + correction des champs à afficher dans easy admin pour l'entité Plan

--- src/Controller/Admin/PlanCrudController.php
+++ src/Controller/Admin/PlanCrudController.php
@@ -9,6 +9,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 
 class PlanCrudController extends AbstractCrudController
@@ -23,9 +24,11 @@ class PlanCrudController extends AbstractCrudController
         return [
             IdField::new('id')->hideOnForm(),
             TextField::new('name', 'Nom du plan'),
-            MoneyField::new('price', 'Prix')->setCurrency('EUR'),
-            IntegerField::new('duration', 'Durée (jours)'),
-            BooleanField::new('isActive', 'Actif'),
+            SlugField::new('slug')->setTargetFieldName('nom'),
+            TextField::new('stripe_id'),
+            MoneyField::new('prix')->setCurrency('EUR'),
+            DateField::new('created_at','Crée le'),
+            TextField::new('payment_link')
         ];
     }
 }
--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -302,4 +302,9 @@ class Users implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return $this->email ? (string) $this->email : '';
+    }
 }
